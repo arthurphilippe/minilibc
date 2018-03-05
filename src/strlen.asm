0aee1ce4fcc11ef47eb1b6ae296df57106a88cce
Arthur Philippe <arthur.philippe@epitech.eu>
2018-03-05 19:41:25 +0100

    no need to use bp

--- src/strlen.asm
+++ src/strlen.asm
@@ -1,21 +1,14 @@
 	global	strlen:function
 	section	.text
 strlen:
-	push	rbp
-	mov	rbp, rsp
-
-	; arguments
-	mov	QWORD [rbp-24], rdi
-
 	mov	rax, 0			; prepare loop counter and ret value
 	jmp	.LOOP
 
 .INC:
 	add	rax, 1
 .LOOP:
-	cmp	BYTE [rbp-24+rax], 0	; str[i] != 0
+	cmp	BYTE [rdi+rax], 0	; str[i] != 0
 	jne	.INC
 
 .END:
-	pop	rbp			; restore base
 	ret
